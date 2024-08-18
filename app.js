55d145fbda5f8defe887b362ab0fbc464c188941
wisdom Agha <wisdomcool11@gmail.com>
2024-08-18 12:34:13 -0700

    firth commit: created the scroll  section before footer

--- app.js
+++ app.js
@@ -21,3 +21,86 @@ toggleMe.forEach((faq)=>{
 
 
 
+// check email
+const email = document.querySelector('#email');
+const emailForm = document.querySelector('#emailForm');
+
+emailForm.addEventListener("submit", e =>{
+    e.preventDefault();
+
+    validateEmail();
+})
+
+const setError = (element, message)=>{
+    const inputError = element.parentElement;
+    const displayError = inputError.querySelector(".error__msg");
+
+    displayError.innerText = message;
+    inputError.classList.add("error");
+    inputError.classList.remove("success");
+}
+
+const setSuccess = element =>{
+    const inputError = element.parentElement;
+    const displayError = inputError.querySelector(".error__msg");
+
+    displayError.innerText = " ";
+    inputError.classList.remove("error");
+    inputError.classList.add("success");
+}
+
+const isValidEmail = email =>{
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(String(email).toLowerCase());
+}
+
+const validateEmail= ()=>{
+
+    const emailValue = email.value.trim();
+
+    
+    if(emailValue === ''){
+        setError(email, "field is required")
+    }else if(!isValidEmail(emailValue)){
+        setError(email, "make sure it's a valid email")
+    }else{
+        setSuccess(email);
+        emailValue = '';// to reset
+    }
+
+}
+
+
+
+//### swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+
+
+
+
